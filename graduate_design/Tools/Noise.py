58e81790900bfd82f2bd0b47d53d06baf34e8678
lyh <1148105005@qq.com>
2021-12-23 21:16:03 +0800

    NoiseGenerated

---
+++ graduate_design/Tools/Noise.py
@@ -0,0 +1,59 @@
+from matplotlib.pyplot import gray
+import skimage
+import numpy as np
+
+
+# 传统的噪声模式
+# image：输入图片（将会被转换成浮点型），ndarray型
+# mode： 可选择，str型，表示要添加的噪声类型
+#  gaussian：高斯噪声
+#  localvar：高斯分布的加性噪声，在“图像”的每个点处具有指定的局部方差。
+#  poisson：泊松噪声
+#  salt：盐噪声，随机将像素值变成1
+#  pepper：椒噪声，随机将像素值变成0或-1，取决于矩阵的值是否带符号
+#  s&p：椒盐噪声
+#  speckle：均匀噪声（均值mean方差variance），out=image+n*image
+# seed： 可选的，int型，如果选择的话，在生成噪声前会先设置随机种子以避免伪随机
+# clip： 可选的，bool型，如果是True，在添加均值，泊松以及高斯噪声后，会将图片的数据裁剪到合适范围内。如果谁False，则输出矩阵的值可能会超出[-1,1]
+# mean： 可选的，float型，高斯噪声和均值噪声中的mean参数，默认值=0
+# var：  可选的，float型，高斯噪声和均值噪声中的方差，默认值=0.01（注：不是标准差）
+# local_vars：可选的，ndarry型，用于定义每个像素点的局部方差，在localvar中使用
+# amount： 可选的，float型，是椒盐噪声所占比例，默认值=0.05
+# salt_vs_pepper：可选的，float型，椒盐噪声中椒盐比例，值越大表示盐噪声越多，默认值=0.5，即椒盐等量
+def traditional_noise(gray_img, mode, *args, **kwargs):
+    ans = skimage.util.random_noise(gray_img, mode = mode, *args, **kwargs)
+    ans = ans*255
+    ans = ans.astype(np.int)
+
+    return ans
+
+
+# 将图片指定百分比的像素替换为随机值
+def random_replace_element(gray_img, percent):
+    w = gray_img.shape[0]
+    h = gray_img.shape[1]
+    num = int(percent*w*h)
+    one_dim = np.reshape(gray_img,w*h)
+    pos = np.random.choice(one_dim.shape[0], num, replace=False)
+    noise = np.random.randint(0,255,size=num)    
+    np.put(one_dim,pos,noise)
+    ans = np.reshape(one_dim, (w,h))
+    return ans
+
+
+# 将图片指定百分比的像素替换为noise_img图片上随机的像素    
+def random_replace_element_from_another_picture(gray_img, noise_img, percent):
+    w = gray_img.shape[0]
+    h = gray_img.shape[1]
+    num = int(percent*w*h)
+    one_dim = np.reshape(gray_img,w*h)
+    pos = np.random.choice(one_dim.shape[0], num, replace=False)    
+    w_noise = noise_img.shape[0]
+    h_noise = noise_img.shape[1]
+    one_dim_noise = np.reshape(noise_img, w_noise*h_noise)
+    noise_pos = np.random.choice(one_dim_noise.shape[0], num, replace=True)
+    noise = np.take(noise_img, noise_pos)    
+    np.put(one_dim, pos, noise)
+    ans = np.reshape(one_dim, (w,h))
+    return ans
+
--- graduate_design/tests/DrawPicture.py
+++ graduate_design/tests/DrawPicture.py
@@ -1,6 +1,7 @@
 from genericpath import getsize
 import sys
 sys.path.append('graduate_design/Characteristics')
+sys.path.append('graduate_design/Tools')
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -14,6 +15,8 @@ import time
 import os
 import tqdm
 import csv
+import Noise
+
 
 
 def get_img(path):
@@ -38,25 +41,22 @@ def fakecolor_and_csv(path_a, path_b, step, size_w, size_h, figsize, foldername,
     csv_label.append('foldername')
     csv_data = []
     csv_data.append(foldername)
-    
-    
-    
-     
+         
     fakecolor_cc = FakeColor_Characteristics.FakeColor_Color_Characteristics(matrix_a, matrix_b, step = step, size_w = size_w, size_h = size_h, folder = folder, figsize=figsize)
     fakecolor_cc.fakecolor_color_characteristics()
     csv_label = csv_label + fakecolor_cc.csv_label
     csv_data  = csv_data  + fakecolor_cc.csv_data
     # print(fakecolor_cc.csv_data)
     # print(fakecolor_cc.csv_label)
-    # fakecolor_tc = FakeColor_Characteristics.FakeColor_Texture_Characteristecs(matrix_a, matrix_b, step = step, size_w = size_w, size_h = size_h, folder = folder, figsize=figsize)
-    # fakecolor_tc.fakecolor_texture_characteristics()
-    # csv_label = csv_label + fakecolor_tc.csv_label
-    # csv_data  = csv_data  + fakecolor_tc.csv_data
+    fakecolor_tc = FakeColor_Characteristics.FakeColor_Texture_Characteristecs(matrix_a, matrix_b, step = step, size_w = size_w, size_h = size_h, folder = folder, figsize=figsize)
+    fakecolor_tc.fakecolor_texture_characteristics()
+    csv_label = csv_label + fakecolor_tc.csv_label
+    csv_data  = csv_data  + fakecolor_tc.csv_data
 
-    # fakecolor_lac = FakeColor_Characteristics.FakeColor_LossAboutColor_Characteristics(img_a, img_b, step = step, size_w = size_w, size_h = size_h, folder = folder, figsize=figsize)
-    # fakecolor_lac.fakecolor_loss_about_color()
-    # csv_label = csv_label + fakecolor_lac.csv_label
-    # csv_data  = csv_data  + fakecolor_lac.csv_data    
+    fakecolor_lac = FakeColor_Characteristics.FakeColor_LossAboutColor_Characteristics(img_a, img_b, step = step, size_w = size_w, size_h = size_h, folder = folder, figsize=figsize)
+    fakecolor_lac.fakecolor_loss_about_color()
+    csv_label = csv_label + fakecolor_lac.csv_label
+    csv_data  = csv_data  + fakecolor_lac.csv_data    
     if not os.path.exists(csv_path):
         print("Create New CSV File!")
         with open(csv_path, "w") as csvfile:
@@ -78,23 +78,55 @@ if __name__=="__main__":
     # for i in tqdm.trange(10):
     #     for j in range(10):
     #         time.sleep(0.25)
-    start_time = time.perf_counter()
-    print('Start!')
+    
+    # start_time = time.perf_counter()
+    # print('Start!')
     
     
-    path_a = 'graduate_design/Data/Model_LS.png'
-    path_b = 'graduate_design/Data/Photo_LS.png'
+    # path_a = 'graduate_design/Data/Model.png'
+    # path_b = 'graduate_design/Data/Photo.png'
 
-    step = 8
-    size_w = 40 
-    size_h = 40
-    figsize = (18,10)
-    fakecolor_foldername = 'GlcmMultithreadTest'
-    fakecolor_folder = '/home/lyh/results/'+fakecolor_foldername+'/'   
-    csv_path = '/home/lyh/results/test.csv'
+    # step = 8
+    # size_w = 40 
+    # size_h = 40
+    # figsize = (18,10)
+    # fakecolor_foldername = 'LightStageFullPic'
+    # fakecolor_folder = '/home/lyh/results/'+fakecolor_foldername+'/'   
+    # csv_path = '/home/lyh/results/lightstage.csv'
    
-    fakecolor_and_csv(path_a, path_b, step, size_w, size_h, figsize, fakecolor_foldername, fakecolor_folder, csv_path)
+    # fakecolor_and_csv(path_a, path_b, step, size_w, size_h, figsize, fakecolor_foldername, fakecolor_folder, csv_path)
     
 
-    end_time = time.perf_counter()  
-    print('程序共运行 {_time_}秒'.format(_time_=(end_time - start_time)))
+    # end_time = time.perf_counter()  
+    # print('程序共运行 {_time_}秒'.format(_time_=(end_time - start_time)))
+
+        
+    path_a = 'graduate_design/Data/eee.jpg'
+    path_b = 'graduate_design/Data/bbb.jpg'
+    img = cv2.imread(path_a)
+    img_r,img_g,img_b = cv2.split(img)
+    
+    noise = cv2.imread(path_b)
+    noise_r, noise_g, noise_b = cv2.split(noise)
+    # # cv2.imshow("origin", img)
+    # # cv2.waitKey(0) 
+    # ans = Noise.traditional_noise(img_r, "gaussian")
+    # print(ans)
+    
+    
+    mat = np.array([[1,4,5,6,7],[2,3,5,9,8]])
+    
+    noise = np.array([[255,255],[66,99]])
+    
+    
+    
+    # ans = Noise.random_replace_element_from_another_picture(mat, noise, 0.5)
+    
+  
+    # ans = Noise.random_replace_element(mat, 4)
+    # ans = Noise.random_replace_element(img_r, 0.99)
+    ans = Noise.random_replace_element_from_another_picture(img_r, noise_r, 0.8)
+    plt.figure()
+    plt.imshow(ans,cmap='gray')
+    plt.show()
+    
