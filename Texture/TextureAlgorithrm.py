e339daa6ff0d77187a20d7312fdb89f984dbabb3
lyh <1148105005@qq.com>
2022-04-14 17:10:16 +0800

    bug fixed

--- Texture/TextureAlgorithrm.py
+++ Texture/TextureAlgorithrm.py
@@ -724,7 +724,6 @@ def  laws_feature(gray_img):
     
     filter_vectors = np.array(  [[1, 4, 6,  4, 1],
                                 [-1, -2, 0, 2, 1],
-                                [-1, 0, 2, 0, -1],
                                 [-1, 0, 2, 0, 1],
                                 [1, -4, 6, -4, 1]])
     filters = list()
